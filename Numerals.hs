3fb09876956cba755238d88d24b9e873fea47270
Roel van Dijk <vandijk.roel@gmail.com>
2009-03-03 17:37:25 +0100

    Solved conflict
    Ignore-this: ca01e402663b12e0bffe7ba552c3a831
    
    darcs-hash:20090303163725-b004c-8df57b43f64404fdd3355f6dca3f54fb0a78168e.gz

--- Numerals.hs
+++ Numerals.hs
@@ -174,28 +174,27 @@ type One s = (Integer, s) -> s
 type Add s = (Integer, s) -> (Integer, s) -> s
 type Mul s = (Integer, s) -> (Integer, s) -> s
 
-d :: Integer -> Integer
-d = (10 ^)
-
 -------------------------------------------------------------------------------
 --
 -------------------------------------------------------------------------------
 
 cardinal :: NumConfig s -> Integer -> Maybe s
-cardinal NumConfig {..} 0 = fmap symStr $ ncCardinal 0
-cardinal NumConfig {..} x | x < 0     = fmap ncNeg $ go $ abs x
-                          | otherwise = go x
-    where go n = do (NumSym _ v _ vs) <- ncCardinal n
-                    case n `divMod` v of
-                      (1, 0) -> return $ ncOne (v, vs)
-                      (1, r) -> do rs <- go r
-                                   return $ (v, ncOne (v, vs)) `ncAdd` (r, rs)
-                      (q, r) | q >= v    -> Nothing
-                             | otherwise -> do qs <- go q
-                                               if r == 0
-                                                 then return $ (q, qs) `ncMul` (v, vs)
-                                                 else do rs <- go r
-                                                         return $ (q*v, (q, qs) `ncMul` (v, vs)) `ncAdd` (r, rs)
+cardinal NumConfig {..} 0 = fmap (\sym -> symRepr sym O) $ ncCardinal 0
+cardinal NumConfig {..} x | x < 0     = fmap ncNeg $ go O $ abs x
+                          | otherwise = go O x
+    where go ctx n = do (NumSym _ v _ rv) <- ncCardinal n
+                        let vs = rv ctx
+                        case n `divMod` v of
+                          (1, 0) -> return $ ncOne (v, vs)
+                          (1, r) -> do rs <- go (RA v) r
+                                       return $ (v, ncOne (v, rv (LA r))) `ncAdd` (r, rs)
+                          (q, r) | q >= v    -> Nothing
+                                 | otherwise -> do qs <- go (LM v) q
+                                                   if r == 0
+                                                     then return $ (q, qs) `ncMul` (v, rv (RM q))
+                                                     else do let qv = q * v
+                                                             rs <- go (RA qv) r
+                                                             return $ (qv, (q, qs) `ncMul` (v, rv (RM q))) `ncAdd` (r, rs)
 
 findSym :: [NumSymbol s] -> Integer -> Maybe (NumSymbol s)
 findSym []     _ = Nothing
@@ -283,49 +282,6 @@ instance IsString ShowS where
 instance IsString PP.Doc where
     fromString = PP.text
 
-----------------------------------------
-
-d :: Integer -> Integer
-d = (10 ^)
-
--------------------------------------------------------------------------------
---
--------------------------------------------------------------------------------
-
-cardinal :: NumConfig s -> Integer -> Maybe s
-cardinal NumConfig {..} 0 = fmap symStr $ ncCardinal 0
-cardinal NumConfig {..} x | x < 0     = fmap ncNeg $ go $ abs x
-                          | otherwise = go x
-    where go n = do (NumSym _ v _ vs) <- ncCardinal n
-                    case n `divMod` v of
-                      (1, 0) -> return $ ncOne (v, vs)
-                      (1, r) -> do rs <- go r
-                                   return $ (v, ncOne (v, vs)) `ncAdd` (r, rs)
-                      (q, r) | q >= v    -> Nothing
-                             | otherwise -> do qs <- go q
-                                               if r == 0
-                                                 then return $ (q, qs) `ncMul` (v, vs)
-                                                 else do rs <- go r
-                                                         return $ (q*v, (q, qs) `ncMul` (v, vs)) `ncAdd` (r, rs)
-
-findSym :: [NumSymbol s] -> Integer -> Maybe (NumSymbol s)
-findSym []     _ = Nothing
-findSym (e:es) n = go e e es
-    where go :: NumSymbol s -> NumSymbol s -> [NumSymbol s] -> Maybe (NumSymbol s)
-          go a m [] = stop a m
-          go a m (x@(NumSym t v _ _) : xs)
-              | v == n    = Just x
-              | otherwise = case t of
-                              Terminal        -> go a m xs
-                              Add | v > n     -> stop a m
-                                  | otherwise -> go x m xs
-                              Mul | v > n     -> stop a m
-                                  | otherwise -> go a x xs
-
-          stop :: NumSymbol s -> NumSymbol s -> Maybe (NumSymbol s)
-          stop a@(NumSym {..}) m | n < symVal + symScope = return a
-                                 | otherwise             = return m
-
 -------------------------------------------------------------------------------
 -- Debug
 -------------------------------------------------------------------------------
@@ -371,6 +327,11 @@ test9 = [0, 9, 10, 19, 90, 99, 100, 109, 119, 190, 199, 900, 909, 919, 990, 999,
 
 -------------------------------------------------------------------------------
 -- Numeral configurations
+-------------------------------------------------------------------------------
+
+d :: Integer -> Integer
+d = (10 ^)
+
 -------------------------------------------------------------------------------
 
 nlNeg :: (IsString s, Joinable s) => Neg s

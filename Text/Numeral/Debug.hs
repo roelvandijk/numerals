b619b81b532f6fdead1d26b4b97f8d42f63fc10d
Roel van Dijk <vandijk.roel@gmail.com>
2011-02-17 00:42:43 +0100

    Updated Swedish
    Ignore-this: 78a2391aaf657a3c1c88af391efa0b1a
    
    darcs-hash:20110216234243-b004c-156905252bfaa6343ca91b15febfa2a73e5b58be.gz

--- Text/Numeral/Debug.hs
+++ Text/Numeral/Debug.hs
@@ -24,6 +24,7 @@ import qualified Text.Numeral.Language.FR as FR
 import qualified Text.Numeral.Language.EN as EN
 import qualified Text.Numeral.Language.EO as EO
 import qualified Text.Numeral.Language.JA as JA
+import qualified Text.Numeral.Language.SV as SV
 
 
 --------------------------------------------------------------------------------
--- Text/Numeral/Language/SV.hs
+++ Text/Numeral/Language/SV.hs
@@ -16,6 +16,7 @@ import Prelude       ( Num, fromInteger )
 
 -- from base-unicode-symbols:
 import Data.Monoid.Unicode ( (⊕) )
+import Data.Ord.Unicode    ( (≥) )
 
 -- from containers:
 import qualified Data.IntMap as IM ( fromList, lookup )
@@ -29,69 +30,55 @@ import Text.Numeral.Pelletier ( scale )
 -- SV
 -------------------------------------------------------------------------------
 
--- Sources:
---   http://www.lysator.liu.se/language/Languages/Swedish/Grammar.html
---   http://en.wikibooks.org/wiki/Swedish/Numerals
---   http://longstrom.com/swedishtoenglish.htm#numbers
-
---   http://www.cs.chalmers.se/~aarne/GF/
---   http://www.cs.chalmers.se/~aarne/GF/lib/resource/norwegian/NumeralNor.gf
-
-
 rules ∷ Rules
 rules = Rules { rsFindRule = findRule rs
-              , rsMulOne   = const False
+              , rsMulOne   = (≥ 100)
               }
     where
       rs = map atom [1..9]
-           ⊕ [ mul  10  10 10 LeftAdd
-             , mul 100 100 10 RightAdd
-             ]
-           ⊕ scale RightAdd 3
+         ⊕ [ add 10 10 10 LeftAdd  True
+           , add 20 20 10 RightAdd True
+           , mul 10 30 10 RightAdd
+           ]
+         ⊕ map atom [11..12]
+         ⊕ [mul 100 100 10 RightAdd]
+         ⊕ scale RightAdd 3
 
-sv ∷ (Monoid s, IsString s) ⇒ NumConfig s
-sv = NumConfig { ncNeg      = ("minus" <+>)
-               , ncOne      = svOne
-               , ncAdd      = svAdd
-               , ncMul      = withSnd (<>)
-               , ncCardinal = findSym svTable
+sv ∷ (IsString s) ⇒ (Rules, Repr s)
+sv = (rules, sv_repr)
+
+sv_repr ∷ (IsString s) ⇒ Repr s
+sv_repr = Repr { reprValue = \n → IM.lookup (fromInteger n) symMap
+               , reprAdd = (⊞)
+               , reprMul = \_ _ → ""
+               , reprZero = "noll"
+               , reprNeg  = "minus"
                }
+    where
+      _ ⊞ _ = ""
 
-svOne ∷ (Monoid s, IsString s) ⇒ (Integer, s) → s
-svOne (v, vs) | v ≥ 100   = "ett" <> vs
-              | otherwise  = vs
+      symMap = IM.fromList
+               [ (1,  const "ett")
+               , (2,  const "två")
+               , (3,  ten   "tre"  "tret" "tret")
+               , (4,  ten   "fyra" "fjor" "fyr")
+               , (5,  const "fem")
+               , (6,  const "sex")
+               , (7,  ten   "sju"  "sjut" "sjut")
+               , (8,  ten   "åtta" "ar"   "åt")
+               , (9,  ten   "nio"  "nit"  "nit")
+               , (10, \c → case c of
+                             RA {} → "ton"
+                             _     → "tio"
+                 )
+               , (11, const "elva")
+               , (12, const "tolv")
+               , (20, const "tjugo")
+               , (100, const "hundra")
+               , (1000, const "tusen")
+               ]
 
-svAdd ∷ (Monoid s, IsString s) ⇒ (Integer, s) → (Integer, s) → s
-svAdd (x, x') (_, y') | x < 20    = y' <> x'
-                      | otherwise = x' <> y'
-
-svTable ∷ (Monoid s, IsString s) ⇒ [NumSymbol s]
-svTable = [ term 0        $ const "noll"
-          , term 1        $ const "ett"
-          , term 2        $ const "två"
-          , term 3        $ tenForms "tre"  "tret" "tret"
-          , term 4        $ tenForms "fyra" "fjor" "fyr"
-          , term 5        $ const "fem"
-          , term 6        $ const "sex"
-          , term 7        $ tenForms "sju"  "sjut" "sjut"
-          , term 8        $ tenForms "åtta" "ar"   "åt"
-          , term 9        $ tenForms "nio"  "nit"  "nit"
-          , mul  10       $ \ctx → case ctx of
-                                     LA {} → "ton"
-                                     _     → "tio"
-          , term 11       $ const "elva"
-          , term 12       $ const "tolv"
-          , add  20    10 $ const "tjugo"
-          , mul  100      $ const "hundra"
-          , mul  (d 3)    $ const "tusen"
-          , mul  (d 6)    $ mulForms "miljon"       "miljoner"
-          , mul  (d 9)    $ mulForms "miljard"      "miljarder"
-          , mul  (d 12)   $ mulForms "biljon"       "biljoner"
-          , mul  (d 15)   $ mulForms "biljard"      "biljarder"
-          , mul  (d 18)   $ mulForms "triljon"      "triljoner"
-          , mul  (d 21)   $ mulForms "triljard"     "triljarder"
-          , mul  (d 24)   $ mulForms "kvadriljon"   "kvadriljoner"
-          , mul  (d 27)   $ mulForms "kvadriljard"  "kvadriljarder"
-          , mul  (d 30)   $ mulForms "kvintriljon"  "kvintriljoner"
-          , mul  (d 33)   $ mulForms "kvintriljard" "kvintriljarder"
-          ]
+      ten n a m = \c → case c of
+                         LA (C 10) _ → a
+                         LM (C 10) _ → m
+                         _           → n

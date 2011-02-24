7c75cc88b82e770a9ba3dd758f6d4acd54e7a25a
Roel van Dijk <vandijk.roel@gmail.com>
2011-02-25 00:11:40 +0100

    Shorter definition for latin rules
    Ignore-this: f2d523f9d200e9225cd0f7406eb84417
    
    darcs-hash:20110224231140-b004c-9574d0858f51b48ebb60f420e4526da7dd3b389c.gz

--- Text/Numeral/Language/LA.hs
+++ Text/Numeral/Language/LA.hs
@@ -14,15 +14,17 @@ module Text.Numeral.Language.LA
 -- from base:
 import Control.Monad ( (>>=) )
 import Data.Bool     ( otherwise )
-import Data.Function ( const )
+import Data.Function ( ($), const )
+import Data.List     ( concat )
 import Data.Maybe    ( Maybe )
 import Data.Monoid   ( Monoid )
 import Data.String   ( IsString )
-import Prelude       ( Integral )
+import Prelude       ( Integral, (+) )
 
 -- from base-unicode-symbols:
 import Data.List.Unicode     ( (∈) )
 import Data.Ord.Unicode      ( (≤) )
+import Data.Monoid.Unicode   ( (⊕) )
 
 -- from containers:
 import qualified Data.Map as M ( fromList, lookup )
@@ -48,29 +50,17 @@ cardinal n = deconstruct findRule n >>= textify cardinalRepr
 findRule ∷ (Integral α, Subtract β) ⇒ FindRule α β
 findRule = mkFindRule rules []
 
--- TODO: doen
-
 rules ∷ (Integral α, Subtract β) ⇒ Rules α β
-rules = [ ((   0,   10), atom)
-        , ((  11,   17), add 10 L)
-        , ((  18,   19), sub 20)
-        , ((  20,   27), mul 10 R L)
-        , ((  28,   29), sub 30)
-        , ((  30,   37), mul 10 R L)
-        , ((  38,   39), sub 40)
-        , ((  40,   47), mul 10 R L)
-        , ((  48,   49), sub 50)
-        , ((  50,   57), mul 10 R L)
-        , ((  58,   59), sub 60)
-        , ((  60,   67), mul 10 R L)
-        , ((  68,   69), sub 70)
-        , ((  70,   77), mul 10 R L)
-        , ((  78,   79), sub 80)
-        , ((  80,   87), mul 10 R L)
-        , ((  88,   89), sub 90)
-        , ((  90,   97), mul 10 R L)
-        , ((  98,   99), sub 100)
-        , (( 100,  100), atom)
+rules = [ (( 0, 10), atom)
+        , ((11, 17), add 10 L)
+        , ((18, 19), sub 20)
+        ]
+      ⊕ concat [ [ ((n,  n+7), mul 10 R L)
+                 , ((n+8,n+9), sub $ n+10)
+                 ]
+               | n ← [20,30..90]
+               ]
+      ⊕ [ (( 100,  100), atom)
         , (( 101,  199), add 100 L)
         , (( 200,  999), mul 100 R L)
         , ((1000, 1000), atom)
@@ -82,7 +72,7 @@ cardinalRepr =
          , reprAdd   = (⊞)
          , reprMul   = (⊡)
          , reprSub   = \_ _ → "dē"
-         -- TODO: negative numbers probably can't be represented in latin
+         -- TODO: negative numbers probably can't be represented in latin.
          , reprNeg   = "- "
          }
     where

bcd4d784f5b5cf7d6b31f63068a93b7bf5c1f39e
Roel van Dijk <vandijk.roel@gmail.com>
2011-02-27 13:42:55 +0100

    Fixed Debug module
    Ignore-this: 974fb9d00a33375ed53acadf86b3fc77
    
    darcs-hash:20110227124255-b004c-d4fe458f3700ac8d65ff3a8ca5e275526cbe3471.gz

--- Text/Numeral/Debug.hs
+++ Text/Numeral/Debug.hs
@@ -9,29 +9,36 @@ module Text.Numeral.Debug where
 
 -- from base:
 import Control.Monad ( forM_ )
+import Data.Function ( fix )
+import Data.Maybe    ( fromMaybe )
 
 -- from base-unicode-symbols:
 import Data.Monoid.Unicode   ( (⊕) )
 
 -- from numerals:
 import Text.Numeral
-import qualified Text.Numeral.Language.NL as NL
-import qualified Text.Numeral.Language.DE as DE
-import qualified Text.Numeral.Language.FR as FR
-import qualified Text.Numeral.Language.EN as EN
-import qualified Text.Numeral.Language.EO as EO
-import qualified Text.Numeral.Language.JA as JA
-import qualified Text.Numeral.Language.SV as SV
-import qualified Text.Numeral.Language.NO as NO
-import qualified Text.Numeral.Language.LA as LA
+import qualified Text.Numeral.Language.CHN as CHN
+import qualified Text.Numeral.Language.DE  as DE
+import qualified Text.Numeral.Language.EN  as EN
+import qualified Text.Numeral.Language.EO  as EO
+import qualified Text.Numeral.Language.FR  as FR
+import qualified Text.Numeral.Language.JA  as JA
+import qualified Text.Numeral.Language.LA  as LA
+import qualified Text.Numeral.Language.NL  as NL
+import qualified Text.Numeral.Language.NO  as NO
+import qualified Text.Numeral.Language.NQM as NQM
+import qualified Text.Numeral.Language.SV  as SV
 
 
 --------------------------------------------------------------------------------
 -- Debug and test stuff
 --------------------------------------------------------------------------------
 
-test ∷ (Integer → Maybe String) → [Integer] → IO ()
-test cardinal xs = forM_ xs $ \x → do putStr $ (show x) ⊕ " - "
-                                      maybe (putStrLn "error")
-                                            putStrLn
-                                            $ cardinal x
+
+test ∷ Rule Integer Exp → Repr String → [Integer] → IO ()
+test rule repr xs = forM_ xs $ \x → do putStr $ (show x) ⊕ " - "
+                                       case (fix rule) x of
+                                         Nothing → putStrLn "error"
+                                         Just e → do putStr $ show e
+                                                     putStr " - "
+                                                     putStrLn $ fromMaybe "error" (textify repr e)

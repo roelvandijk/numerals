0af610fe361578b587465cacfea853c7492c76db
Roel van Dijk <vandijk.roel@gmail.com>
2011-03-22 23:51:29 +0100

    Added Scots (SCO)
    Ignore-this: b5a6a8da222cf84190ffe86934d8e9c6
    
    darcs-hash:20110322225129-cab80-87ed4492222079640454131d0d6e7e10cca386fe.gz

---
+++ src/Text/Numeral/Language/SCO.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, UnicodeSyntax #-}
+
+{-|
+[@ISO639-1@]        -
+
+[@ISO639-2@]        sco
+
+[@ISO639-3@]        sco
+
+[@Native name@]     Scots
+
+[@English name@]    Scots
+-}
+
+module Text.Numeral.Language.SCO
+    ( cardinal
+    , struct
+    , cardinalRepr
+    ) where
+
+
+-------------------------------------------------------------------------------
+-- Imports
+-------------------------------------------------------------------------------
+
+-- from base:
+import Control.Monad ( (>=>) )
+import Data.Function ( ($), const, fix )
+import Data.Maybe    ( Maybe(Just) )
+import Data.Monoid   ( Monoid )
+import Data.String   ( IsString )
+import Prelude       ( Integral )
+
+-- from containers:
+import qualified Data.Map as M ( fromList, lookup )
+
+-- from numerals:
+import Text.Numeral
+import qualified Text.Numeral.Exp.Classes as C
+
+
+-------------------------------------------------------------------------------
+-- SCO
+-------------------------------------------------------------------------------
+
+{-
+Sources:
+  http://www.sf.airnet.ne.jp/~ts/language/number/scots.html
+-}
+
+cardinal ∷ (Monoid s, IsString s, Integral α) ⇒ α → Maybe s
+cardinal = struct >=> cardinalRepr
+
+struct ∷ (Integral α, C.Lit β, C.Add β, C.Mul β) ⇒ α → Maybe β
+struct = checkPos
+       $ fix
+       $ findRule (  1, lit       )
+                [ ( 13, add 10 L  )
+                , ( 20, mul 10 R L)
+                , (100, lit       )
+                ]
+                   100
+
+cardinalRepr ∷ (Monoid s, IsString s) ⇒ Exp → Maybe s
+cardinalRepr = textify defaultRepr
+               { reprValue = \n → M.lookup n symMap
+               , reprAdd   = (⊞)
+               , reprMul   = \_ _ → Just ""
+               }
+    where
+      (_ `Mul` _) ⊞ _ = Just " "
+      _           ⊞ _ = Just ""
+
+      symMap = M.fromList
+               [ (1, const    "ane"                     )
+               , (2, tenForms "twa"    "twa"    "twin"  )
+               , (3, tenForms "three"  "ther"   "ther"  )
+               , (4, const    "fower"                   )
+               , (5, tenForms "five"   "feif"   "fuf"   )
+               , (6, const    "sax"                     )
+               , (7, tenForms "seeven" "seiven" "seeven")
+               , (8, tenForms "echt"   "ech"    "ech"   )
+               , (9, tenForms "nine"   "nin"    "nin"   )
+               , (10, \c → case c of
+                             CtxAdd {} → "teen"
+                             CtxMul {} → "tie"
+                             _         → "ten"
+                 )
+               , (11, const "aleeven")
+               , (12, const "twal")
+               , (100, const "hunner")
+               , (1000, const "thousant")
+               ]
+
+      tenForms o a m = \c → case c of
+                              CtxAdd L _ _ → a
+                              CtxMul {}    → m
+                              _            → o
--- test/test.hs
+++ test/test.hs
@@ -75,6 +75,8 @@ import qualified Text.Numeral.Language.PAA          as PAA ( cardinal  )
 import qualified Text.Numeral.Language.PAA.TestData as PAA ( cardinals )
 import qualified Text.Numeral.Language.PT           as PT  ( cardinal  )
 import qualified Text.Numeral.Language.PT.TestData  as PT  ( cardinals )
+import qualified Text.Numeral.Language.SCO          as SCO ( cardinal  )
+import qualified Text.Numeral.Language.SCO.TestData as SCO ( cardinals )
 import qualified Text.Numeral.Language.SV           as SV  ( cardinal  )
 import qualified Text.Numeral.Language.SV.TestData  as SV  ( cardinals )
 import qualified Text.Numeral.Language.YOR          as YOR ( cardinal  )
@@ -141,6 +143,7 @@ tests = [ testGroup "AMP" [mkTests "cardinal" AMP.cardinal AMP.cardinals]
         , testGroup "NQM" [mkTests "cardinal" NQM.cardinal NQM.cardinals]
         , testGroup "PAA" [mkTests "cardinal" PAA.cardinal PAA.cardinals]
         , testGroup "PT"  [mkTests "cardinal" PT.cardinal  PT.cardinals]
+        , testGroup "SCO" [mkTests "cardinal" SCO.cardinal SCO.cardinals]
         , testGroup "SV"  [mkTests "cardinal" SV.cardinal  SV.cardinals]
         , testGroup "YOR" [mkTests "cardinal" YOR.cardinal YOR.cardinals]
         ]

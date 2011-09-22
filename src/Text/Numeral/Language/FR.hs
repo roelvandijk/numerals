4ab41c98e1d0254157e0919a7be20241da5348e9
Roel van Dijk <vandijk.roel@gmail.com>
2011-09-22 23:03:37 +0200

    French (FR) ordinals

--- src/Text/Numeral/Language/FR.hs
+++ src/Text/Numeral/Language/FR.hs
@@ -18,7 +18,9 @@
 
 module Text.Numeral.Language.FR
     ( cardinal
-    , struct
+    , ordinal
+    , cardinalStruct
+    , ordinalStruct
     ) where
 
 
@@ -32,9 +34,10 @@ import "base" Data.Maybe    ( Maybe(Just) )
 import "base" Data.Monoid   ( Monoid )
 import "base" Data.Ord      ( (<) )
 import "base" Data.String   ( IsString )
-import "base" Prelude       ( Integral, (-), Integer )
+import "base" Prelude       ( Integral, (-) )
 import "base-unicode-symbols" Data.Function.Unicode ( (∘) )
 import "base-unicode-symbols" Data.Ord.Unicode ( (≤), (≥) )
+import "base-unicode-symbols" Prelude.Unicode ( ℤ )
 import qualified "containers" Data.Map as M ( fromList, lookup )
 import           "numerals-base" Text.Numeral
 import           "numerals-base" Text.Numeral.Misc ( dec )
@@ -50,39 +53,48 @@ import qualified "numerals-base" Text.Numeral.Exp.Classes as C
 Sources:
   http://www.sf.airnet.ne.jp/~ts/language/number/french.html
   http://www.french-linguistics.co.uk/tutorials/numbers/
+  http://www.parisbypod.com/2007/10/23/french-ordinal-numbers/
 -}
 
 cardinal ∷ (Integral α, C.Scale α, Monoid s, IsString s) ⇒ α → Maybe s
-cardinal = cardinalRepr ∘ struct
+cardinal = cardinalRepr ∘ cardinalStruct
 
-struct ∷ ( Integral α, C.Scale α
-         , C.Unknown β, C.Lit β, C.Neg β, C.Add β, C.Mul β, C.Scale β
-         )
-       ⇒ α → β
-struct = pos $ fix $ rule `combine` pelletierScale1 R L BN.rule
-    where
-      rule = findRule (   0, lit         )
-                    [ (  11, add   10 L  )
-                    , (  17, add   10 R  )
-                    , (  20, lit         )
-                    , (  21, add   20 R  )
-                    , (  30, mul   10 R L)
-                    , (  70, add   60 R  )
-                    , (  80, mul   20 R L)
-                    , (  89, add   80 R  )
-                    , ( 100, step  100   10 R L)
-                    , (1000, step 1000 1000 R L)
-                    ]
-                      (dec 6 - 1)
+ordinal ∷ (Integral α, C.Scale α, Monoid s, IsString s) ⇒ α → Maybe s
+ordinal = ordinalRepr ∘ ordinalStruct
 
-cardinalRepr ∷ (Monoid s, IsString s) ⇒ Exp → Maybe s
-cardinalRepr = render defaultRepr
-               { reprValue = \n → M.lookup n syms
-               , reprScale = pelletierRepr
-               , reprAdd   = Just (⊞)
-               , reprMul   = Just (⊡)
-               , reprNeg   = Just $ \_ _ → "moins "
-               }
+cardinalStruct ∷ ( Integral α, C.Scale α
+                 , C.Unknown β, C.Lit β, C.Neg β, C.Add β, C.Mul β, C.Scale β
+                 )
+               ⇒ α → β
+cardinalStruct = pos $ fix $ rule `combine` pelletierScale1 R L BN.rule
+
+ordinalStruct ∷ ( Integral α, C.Scale α
+                , C.Unknown β, C.Lit β, C.Neg β, C.Add β, C.Mul β, C.Scale β
+                )
+              ⇒ α → β
+ordinalStruct = pos $ fix $ rule `combine` pelletierScale R L BN.rule
+
+rule ∷ (Integral α, C.Unknown β, C.Lit β, C.Add β, C.Mul β) ⇒ Rule α β
+rule = findRule (   0, lit         )
+              [ (  11, add   10 L  )
+              , (  17, add   10 R  )
+              , (  20, lit         )
+              , (  21, add   20 R  )
+              , (  30, mul   10 R L)
+              , (  70, add   60 R  )
+              , (  80, mul   20 R L)
+              , (  89, add   80 R  )
+              , ( 100, step  100   10 R L)
+              , (1000, step 1000 1000 R L)
+              ]
+                (dec 6 - 1)
+
+genericRepr ∷ (Monoid s, IsString s) ⇒ Repr s
+genericRepr = defaultRepr
+              { reprAdd   = Just (⊞)
+              , reprMul   = Just (⊡)
+              , reprNeg   = Just $ \_ _ → "moins "
+              }
     where
       (Lit n                ⊞ Lit 10) _       | n ≤ 6 = ""
       (Lit 10               ⊞ Lit n ) _       | n ≥ 7 = "-"
@@ -97,6 +109,14 @@ cardinalRepr = render defaultRepr
       (_ ⊡ Lit 20) _ = "-"
       (_ ⊡ _     ) _ = " "
 
+cardinalRepr ∷ (Monoid s, IsString s) ⇒ Exp → Maybe s
+cardinalRepr = render genericRepr
+               { reprValue = \n → M.lookup n syms
+               , reprScale = BN.pelletierRepr (BN.quantityName "illion"  "illions")
+                                              (BN.quantityName "illiard" "illiards")
+                                              bigNumSyms
+               }
+    where
       syms =
           M.fromList
           [ (0, const "zéro")
@@ -132,17 +152,80 @@ cardinalRepr = render defaultRepr
                         CtxMul _ (Lit 10) _ → m
                         _                   → n
 
-pelletierRepr ∷ (IsString s, Monoid s)
-              ⇒ Integer → Integer → Exp → Ctx Exp → Maybe s
-pelletierRepr = BN.pelletierRepr
-                  (BN.quantityName "illion"  "illions")
-                  (BN.quantityName "illiard" "illiards")
-                  [ (1, BN.forms "m"  "uno" "uno"  ""    "")
-                  , (3, BN.forms "tr" "tré" "tres" "tri" "tre")
-                  , (10, \c → case c of
-                                CtxAdd _ (Lit 100) _              → "deci"
-                                CtxMul _ _ (CtxAdd _ (Lit 100) _) → "ginta"
-                                CtxMul {}                         → "gint"
-                                _                                 → "déc"
-                    )
-                  ]
+ordinalRepr ∷ (Monoid s, IsString s) ⇒ Exp → Maybe s
+ordinalRepr = render genericRepr
+              { reprValue = \n → M.lookup n syms
+              , reprScale = BN.pelletierRepr ( BN.ordQuantityName "illion"  "illionième"
+                                                                  "illions" "illionième"
+                                             )
+                                             ( BN.ordQuantityName "illiard"  "illiardième"
+                                                                  "illiards" "illiardième"
+                                             )
+                                             bigNumSyms
+              }
+    where
+      syms =
+          M.fromList
+          [ (0, \c → case c of
+                       CtxEmpty → "zéroth"
+                       _        → "zéro"
+            )
+          , (1, \c → case c of
+                       CtxEmpty → "premier"
+                       CtxAdd _ (Lit 10) _ → "on"
+                       CtxMul _ (Lit 10) _ → "un"
+                       _                   | isOutside R c → "unième"
+                                           | otherwise     → "un"
+            )
+          , (2, ten   "deuxième"  "deux"   "dou"    "deux")
+          , (3, ten   "troisième" "trois"  "trei"   "tren")
+          , (4, ten   "quatrième" "quatre" "quator" "quar")
+          , (5, ten   "cinquième" "cinq"   "quin"   "cinqu")
+          , (6, ten   "sixième"   "six"    "sei"    "soix")
+          , (7, \c → if isOutside R c then "septième" else "sept")
+          , (8, \c → if isOutside R c then "huitième" else "huit")
+          , (9, \c → if isOutside R c then "neuvième" else "neuf")
+          , (10, \c → case c of
+                        CtxAdd R (Lit _) _ | isOutside R c → "zième"
+                                           | otherwise     → "ze"
+                        CtxAdd L (Lit _) _                 → "dix"
+                        CtxMul _ (Lit 3) _ | isOutside R c → "tième"
+                                           | otherwise     → "te"
+                        CtxMul R _       _ | isOutside R c → "antième"
+                                           | otherwise     → "ante"
+                        _                  | isOutside R c → "dixième"
+                                           | otherwise     → "dix"
+            )
+          , (20, \c → case c of
+                        _ | isOutside R c → "vingtième"
+                        CtxMul _ _ CtxEmpty → "vingts"
+                        _                   → "vingt"
+            )
+          , (100, \c → case c of
+                         _ | isOutside R c   → "centième"
+                         CtxMul R _ CtxEmpty → "cents"
+                         _                   → "cent"
+            )
+          , (1000, \c → if isOutside R c then "millième" else "mille")
+          ]
+
+      ten o n a m ctx =
+          case ctx of
+            CtxAdd _ (Lit 10) _ | isOutside R ctx → o
+                                | otherwise       → a
+            CtxMul _ (Lit 10) _ | isOutside R ctx → o
+                                | otherwise       → m
+            _                   | isOutside R ctx → o
+                                | otherwise       → n
+
+bigNumSyms ∷ (IsString s) ⇒ [(ℤ, Ctx Exp → s)]
+bigNumSyms =
+    [ (1, BN.forms "m"  "uno" "uno"  ""    "")
+    , (3, BN.forms "tr" "tré" "tres" "tri" "tre")
+    , (10, \c → case c of
+                  CtxAdd _ (Lit 100) _              → "deci"
+                  CtxMul _ _ (CtxAdd _ (Lit 100) _) → "ginta"
+                  CtxMul {}                         → "gint"
+                  _                                 → "déc"
+      )
+    ]
--- test/test.hs
+++ test/test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax #-}
+{-# LANGUAGE NoImplicitPrelude, PackageImports, UnicodeSyntax #-}
 
 module Main where
 
@@ -7,85 +7,75 @@ module Main where
 -- Imports
 --------------------------------------------------------------------------------
 
--- from base:
-import Control.Monad ( when )
-import Data.Function ( ($) )
-import Data.List     ( map )
-import Data.Maybe    ( Maybe(Just), fromMaybe )
-import Prelude       ( Integer, String )
-import System.IO     ( IO )
-import Text.Printf   ( printf )
-import Text.Show     ( show )
-
--- from base-unicode-symbols:
-import Data.Eq.Unicode ( (≢) )
-
--- from HUnit:
-import Test.HUnit ( Assertion, assertFailure )
-
--- from test-framework:
-import Test.Framework ( Test, defaultMain, testGroup )
-
--- from test-framework-hunit:
-import Test.Framework.Providers.HUnit ( testCase )
-
--- from numerals:
-import qualified Text.Numeral.Language.AMP          as AMP
-import qualified Text.Numeral.Language.AMP.TestData as AMP
-import qualified Text.Numeral.Language.CHN          as CHN
-import qualified Text.Numeral.Language.CHN.TestData as CHN
-import qualified Text.Numeral.Language.CHR          as CHR
-import qualified Text.Numeral.Language.CHR.TestData as CHR
-import qualified Text.Numeral.Language.DE           as DE
-import qualified Text.Numeral.Language.DE.TestData  as DE
-import qualified Text.Numeral.Language.EN           as EN
-import qualified Text.Numeral.Language.EN.TestData  as EN
-import qualified Text.Numeral.Language.EO           as EO
-import qualified Text.Numeral.Language.EO.TestData  as EO
-import qualified Text.Numeral.Language.ES           as ES
-import qualified Text.Numeral.Language.ES.TestData  as ES
-import qualified Text.Numeral.Language.FR           as FR
-import qualified Text.Numeral.Language.FR.TestData  as FR
-import qualified Text.Numeral.Language.GV           as GV
-import qualified Text.Numeral.Language.GV.TestData  as GV
-import qualified Text.Numeral.Language.HE           as HE
-import qualified Text.Numeral.Language.HE.TestData  as HE
-import qualified Text.Numeral.Language.IT           as IT
-import qualified Text.Numeral.Language.IT.TestData  as IT
-import qualified Text.Numeral.Language.JA           as JA
-import qualified Text.Numeral.Language.JA.TestData  as JA
-import qualified Text.Numeral.Language.LA           as LA
-import qualified Text.Numeral.Language.LA.TestData  as LA
-import qualified Text.Numeral.Language.MG           as MG
-import qualified Text.Numeral.Language.MG.TestData  as MG
-import qualified Text.Numeral.Language.NL           as NL
-import qualified Text.Numeral.Language.NL.TestData  as NL
-import qualified Text.Numeral.Language.NO           as NO
-import qualified Text.Numeral.Language.NO.TestData  as NO
-import qualified Text.Numeral.Language.NQM          as NQM
-import qualified Text.Numeral.Language.NQM.TestData as NQM
-import qualified Text.Numeral.Language.OJ           as OJ
-import qualified Text.Numeral.Language.OJ.TestData  as OJ
--- import qualified Text.Numeral.Language.PAA          as PAA
--- import qualified Text.Numeral.Language.PAA.TestData as PAA
-import qualified Text.Numeral.Language.PL           as PL
-import qualified Text.Numeral.Language.PL.TestData  as PL
-import qualified Text.Numeral.Language.PT           as PT
-import qualified Text.Numeral.Language.PT.TestData  as PT
-import qualified Text.Numeral.Language.RU           as RU
-import qualified Text.Numeral.Language.RU.TestData  as RU
-import qualified Text.Numeral.Language.SCO          as SCO
-import qualified Text.Numeral.Language.SCO.TestData as SCO
-import qualified Text.Numeral.Language.SV           as SV
-import qualified Text.Numeral.Language.SV.TestData  as SV
-import qualified Text.Numeral.Language.TR           as TR
-import qualified Text.Numeral.Language.TR.TestData  as TR
-import qualified Text.Numeral.Language.WO           as WO
-import qualified Text.Numeral.Language.WO.TestData  as WO
-import qualified Text.Numeral.Language.YOR          as YOR
-import qualified Text.Numeral.Language.YOR.TestData as YOR
-import qualified Text.Numeral.Language.ZH           as ZH
-import qualified Text.Numeral.Language.ZH.TestData  as ZH
+import "base" Control.Monad ( when )
+import "base" Data.Function ( ($) )
+import "base" Data.List     ( map )
+import "base" Data.Maybe    ( Maybe(Just), fromMaybe )
+import "base" Prelude       ( String )
+import "base" System.IO     ( IO )
+import "base" Text.Printf   ( printf )
+import "base" Text.Show     ( show )
+import "base-unicode-symbols" Data.Eq.Unicode ( (≢) )
+import "base-unicode-symbols" Prelude.Unicode ( ℤ )
+import "HUnit" Test.HUnit ( Assertion, assertFailure )
+import "test-framework" Test.Framework ( Test, defaultMain, testGroup )
+import "test-framework-hunit" Test.Framework.Providers.HUnit ( testCase )
+import qualified "this" Text.Numeral.Language.AMP          as AMP
+import qualified "this" Text.Numeral.Language.AMP.TestData as AMP
+import qualified "this" Text.Numeral.Language.CHN          as CHN
+import qualified "this" Text.Numeral.Language.CHN.TestData as CHN
+import qualified "this" Text.Numeral.Language.CHR          as CHR
+import qualified "this" Text.Numeral.Language.CHR.TestData as CHR
+import qualified "this" Text.Numeral.Language.DE           as DE
+import qualified "this" Text.Numeral.Language.DE.TestData  as DE
+import qualified "this" Text.Numeral.Language.EN           as EN
+import qualified "this" Text.Numeral.Language.EN.TestData  as EN
+import qualified "this" Text.Numeral.Language.EO           as EO
+import qualified "this" Text.Numeral.Language.EO.TestData  as EO
+import qualified "this" Text.Numeral.Language.ES           as ES
+import qualified "this" Text.Numeral.Language.ES.TestData  as ES
+import qualified "this" Text.Numeral.Language.FR           as FR
+import qualified "this" Text.Numeral.Language.FR.TestData  as FR
+import qualified "this" Text.Numeral.Language.GV           as GV
+import qualified "this" Text.Numeral.Language.GV.TestData  as GV
+import qualified "this" Text.Numeral.Language.HE           as HE
+import qualified "this" Text.Numeral.Language.HE.TestData  as HE
+import qualified "this" Text.Numeral.Language.IT           as IT
+import qualified "this" Text.Numeral.Language.IT.TestData  as IT
+import qualified "this" Text.Numeral.Language.JA           as JA
+import qualified "this" Text.Numeral.Language.JA.TestData  as JA
+import qualified "this" Text.Numeral.Language.LA           as LA
+import qualified "this" Text.Numeral.Language.LA.TestData  as LA
+import qualified "this" Text.Numeral.Language.MG           as MG
+import qualified "this" Text.Numeral.Language.MG.TestData  as MG
+import qualified "this" Text.Numeral.Language.NL           as NL
+import qualified "this" Text.Numeral.Language.NL.TestData  as NL
+import qualified "this" Text.Numeral.Language.NO           as NO
+import qualified "this" Text.Numeral.Language.NO.TestData  as NO
+import qualified "this" Text.Numeral.Language.NQM          as NQM
+import qualified "this" Text.Numeral.Language.NQM.TestData as NQM
+import qualified "this" Text.Numeral.Language.OJ           as OJ
+import qualified "this" Text.Numeral.Language.OJ.TestData  as OJ
+-- import qualified "this" Text.Numeral.Language.PAA          as PAA
+-- import qualified "this" Text.Numeral.Language.PAA.TestData as PAA
+import qualified "this" Text.Numeral.Language.PL           as PL
+import qualified "this" Text.Numeral.Language.PL.TestData  as PL
+import qualified "this" Text.Numeral.Language.PT           as PT
+import qualified "this" Text.Numeral.Language.PT.TestData  as PT
+import qualified "this" Text.Numeral.Language.RU           as RU
+import qualified "this" Text.Numeral.Language.RU.TestData  as RU
+import qualified "this" Text.Numeral.Language.SCO          as SCO
+import qualified "this" Text.Numeral.Language.SCO.TestData as SCO
+import qualified "this" Text.Numeral.Language.SV           as SV
+import qualified "this" Text.Numeral.Language.SV.TestData  as SV
+import qualified "this" Text.Numeral.Language.TR           as TR
+import qualified "this" Text.Numeral.Language.TR.TestData  as TR
+import qualified "this" Text.Numeral.Language.WO           as WO
+import qualified "this" Text.Numeral.Language.WO.TestData  as WO
+import qualified "this" Text.Numeral.Language.YOR          as YOR
+import qualified "this" Text.Numeral.Language.YOR.TestData as YOR
+import qualified "this" Text.Numeral.Language.ZH           as ZH
+import qualified "this" Text.Numeral.Language.ZH.TestData  as ZH
 
 
 --------------------------------------------------------------------------------
@@ -95,7 +85,7 @@ import qualified Text.Numeral.Language.ZH.TestData  as ZH
 main ∷ IO ()
 main = defaultMain tests
 
-testConversion ∷ (Integer → Maybe String) → Integer → String → Assertion
+testConversion ∷ (ℤ → Maybe String) → ℤ → String → Assertion
 testConversion f n s =
   let r = f n
   in when (r ≢ Just s)
@@ -105,7 +95,7 @@ testConversion f n s =
                  s
                  (fromMaybe "no result" r)
 
-mkTests ∷ String → (Integer → Maybe String) → [(Integer, String)] → Test
+mkTests ∷ String → (ℤ → Maybe String) → [(ℤ, String)] → Test
 mkTests name f xs = testGroup name $ map mkTest xs
     where
       mkTest (n, s) = testCase (show n) $ testConversion f n s
@@ -130,7 +120,10 @@ tests = [ testGroup "AMP" [mkTests "cardinal" AMP.cardinal AMP.cardinals]
           ]
         , testGroup "EO"  [mkTests "cardinal" EO.cardinal EO.cardinals]
         , testGroup "ES"  [mkTests "cardinal" ES.cardinal  ES.cardinals]
-        , testGroup "FR"  [mkTests "cardinal" FR.cardinal FR.cardinals]
+        , testGroup "FR"
+          [ mkTests "cardinal" FR.cardinal FR.cardinals
+          , mkTests "ordinal"  FR.ordinal  FR.ordinals
+          ]
         , testGroup "GV"  [mkTests "cardinal" GV.cardinal GV.cardinals]
         , testGroup "HE"
           [ testGroup "cardinal"

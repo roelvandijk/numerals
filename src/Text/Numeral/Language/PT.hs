9d968e23a3ad9a360bb1c424146fb5b03a9d8930
Roel van Dijk <vandijk.roel@gmail.com>
2011-10-08 20:07:53 +0200

    Support gender in Portuguese (PT) cardinals and added ordinals

--- src/Text/Numeral/Language/PT.hs
+++ src/Text/Numeral/Language/PT.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE NoImplicitPrelude
+{-# LANGUAGE FlexibleContexts
+           , NoImplicitPrelude
            , OverloadedStrings
            , PackageImports
            , UnicodeSyntax
@@ -19,8 +20,10 @@
 module Text.Numeral.Language.PT
     ( -- * Conversions
       cardinal
+    , ordinal
       -- * Structure
-    , struct
+    , cardinal_struct
+    , ordinal_struct
       -- * Bounds
     , bounds
     ) where
@@ -38,48 +41,88 @@ import "base" Data.String   ( IsString )
 import "base" Prelude       ( Integral, (-), negate )
 import "base-unicode-symbols" Data.Eq.Unicode       ( (≡) )
 import "base-unicode-symbols" Data.Function.Unicode ( (∘) )
+import "base-unicode-symbols" Data.List.Unicode     ( (∉) )
+import "base-unicode-symbols" Data.Monoid.Unicode   ( (⊕) )
 import "base-unicode-symbols" Data.Ord.Unicode      ( (≤) )
 import "base-unicode-symbols" Prelude.Unicode       ( ℤ )
 import qualified "containers" Data.Map as M ( fromList, lookup )
 import           "numerals-base" Text.Numeral
 import           "numerals-base" Text.Numeral.Misc ( dec )
-import qualified "numerals-base" Text.Numeral.BigNum as BN
-import qualified "numerals-base" Text.Numeral.Exp    as E
+import qualified "numerals-base" Text.Numeral.BigNum  as BN
+import qualified "numerals-base" Text.Numeral.Exp     as E
+import           "numerals-base" Text.Numeral.Grammar as G
 
 
 -------------------------------------------------------------------------------
--- PT
 -------------------------------------------------------------------------------
 
 -- Sources:
 --   http://www.sonia-portuguese.com/text/numerals.htm
 --   http://www.smartphrase.com/Portuguese/po_numbers_voc.shtml
 
-cardinal ∷ (Integral α, E.Scale α, Monoid s, IsString s) ⇒ i → α → Maybe s
-cardinal inf = cardinalRepr inf ∘ struct
+cardinal ∷ ( G.Feminine i, G.Masculine i
+           , Integral α, E.Scale α
+           , Monoid s, IsString s
+           )
+         ⇒ i → α → Maybe s
+cardinal inf = cardinalRepr inf ∘ cardinal_struct
 
-struct ∷ ( Integral α, E.Scale α
+ordinal ∷ ( G.Feminine i, G.Masculine i, G.Singular i
+          , Integral α, E.Scale α
+          , Monoid s, IsString s
+          )
+         ⇒ i → α → Maybe s
+ordinal inf = ordinalRepr inf ∘ ordinal_struct
+
+cardinal_struct ∷ ( Integral α, E.Scale α
          , E.Unknown β, E.Lit β, E.Neg β, E.Add β, E.Mul β, E.Scale β
+         , E.Inflection β, G.Masculine (E.Inf β)
          )
        ⇒ α → β
-struct = pos
-       $ fix
-       $ findRule (   0, lit       )
-                [ (  11, add 10 L  )
-                , (  16, add 10 R  )
-                , (  20, mul 10 R L)
-                , ( 100, step  100   10 R L)
-                , (1000, step 1000 1000 R L)
-                ]
-                  (dec 6 - 1)
-       `combine` shortScale R L BN.rule
+cardinal_struct = pos $ fix $ rule `combine` shortScale1_pt
+    where
+      rule = findRule (   0, lit       )
+                    [ (  11, add 10 L  )
+                    , (  16, add 10 R  )
+                    , (  20, mul 10 R L)
+                    , ( 100, step  100   10 R L)
+                    , (1000, step 1000 1000 R L)
+                    ]
+                      (dec 6 - 1)
+
+ordinal_struct ∷ ( Integral α, E.Scale α
+         , E.Unknown β, E.Lit β, E.Neg β, E.Add β, E.Mul β, E.Scale β
+         , E.Inflection β, G.Masculine (E.Inf β)
+         )
+       ⇒ α → β
+ordinal_struct = pos $ fix $ rule `combine` shortScale1_pt
+    where
+      rule = findRule (   0, lit       )
+                    [ (  11, add 10 R  )
+                    , (  20, mul 10 R L)
+                    , ( 100, step  100   10 R L)
+                    , (1000, step 1000 1000 R L)
+                    ]
+                      (dec 6 - 1)
+
+-- | Like 'shortScale1' with the difference that all scale elements
+-- are masculine.
+shortScale1_pt ∷ ( Integral α, E.Scale α
+                , E.Unknown β, E.Lit β, E.Add β, E.Mul β, E.Scale β
+                , E.Inflection β, G.Masculine (E.Inf β)
+                )
+              ⇒ Rule α β
+shortScale1_pt = mulScale1_es 3 3 R L BN.rule
+    where
+      mulScale1_es = mulScale_ $ \f m s _ → masculineMul (f m) s
+      masculineMul x y = E.inflection (G.masculine) $ E.mul x y
 
 bounds ∷ (Integral α) ⇒ (α, α)
 bounds = let x = dec 60000 - 1 in (negate x, x)
 
-cardinalRepr ∷ (Monoid s, IsString s) ⇒ i → Exp i → Maybe s
+cardinalRepr ∷ (G.Feminine i, Monoid s, IsString s) ⇒ i → Exp i → Maybe s
 cardinalRepr = render defaultRepr
-               { reprValue = \_ n → M.lookup n syms
+               { reprValue = \inf n → M.lookup n (syms inf)
                , reprScale = shortScaleRepr
                , reprAdd   = Just (⊞)
                , reprMul   = Just (⊡)
@@ -97,18 +140,20 @@ cardinalRepr = render defaultRepr
       (_ ⊡ Lit 100) _ = ""
       (_ ⊡ _      ) _ = " "
 
-      syms =
+      syms inf =
           M.fromList
           [ (0, const "zero")
           , (1, \c → case c of
-                       CtxAdd _ (Lit 10) _ → "on"
-                       _                   → "um"
+                       CtxAdd _ (Lit 10) _  → "on"
+                       _ | G.isFeminine inf → "uma"
+                         | otherwise        → "um"
             )
           , (2, \c → case c of
                        CtxAdd _ (Lit 10)  _ → "do"
                        CtxMul _ (Lit 10)  _ → "vin"
                        CtxMul _ (Lit 100) _ → "duz"
-                       _                    → "dois"
+                       _ | G.isFeminine inf → "duas"
+                         | otherwise        → "dois"
             )
           , (3, \c → case c of
                        CtxAdd _ (Lit 10)  _ → "tre"
@@ -150,16 +195,104 @@ cardinalRepr = render defaultRepr
                         _                  → "dez"
             )
           , (100, \c → case c of
-                         CtxAdd {}       → "cento"
+                         CtxAdd {} → "cento"
                          CtxMul _ (Lit n) _
-                             | n ≤ 3     → "entos"
-                             | n ≡ 4     → "centos"
-                             | n ≡ 5     → "hentos"
-                             | otherwise → "centos"
-                         _               → "cem"
+                             | n ≤ 3 → if G.isFeminine inf then "entas"  else "entos"
+                             | n ≡ 5 → if G.isFeminine inf then "hentas" else "hentos"
+                             | n ≤ 9 → if G.isFeminine inf then "centas" else "centos"
+                             | otherwise → "cem"
+                         _ → "cem"
             )
           , (1000, const "mil")
           ]
+
+ordinalRepr ∷ (G.Feminine i, G.Singular i, Monoid s, IsString s)
+            ⇒ i → Exp i → Maybe s
+ordinalRepr = render defaultRepr
+              { reprValue = \inf n → M.lookup n (syms inf)
+              , reprScale = shortScaleRepr
+              , reprAdd   = Just (⊞)
+              , reprMul   = Just (⊡)
+              , reprNeg   = Just $ \_ _ → "menos "
+              }
+    where
+      (Lit _  ⊞ Lit 10) _ = ""
+      (_      ⊞ _     ) _ = " "
+
+      (_ ⊡ Lit 10 ) _ = ""
+      (_ ⊡ Lit 100) _ = ""
+      (_ ⊡ _      ) _ = " "
+
+      syms inf =
+          M.fromList
+          [ (0, const "zero")
+          , (1, \c → case c of
+                       _ → "primeir" ⊕ postFix
+            )
+          , (2, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "vi"
+                       CtxMul _ (Lit 100) _ → "du"
+                       _                    → "segund" ⊕ postFix
+            )
+          , (3, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "tri"
+                       CtxMul _ (Lit 100) _ → "tre"
+                       _                    → "terceir" ⊕ postFix
+            )
+          , (4, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "quadra"
+                       CtxMul _ (Lit 100) _ → "quadrin"
+                       _                    → "quart" ⊕ postFix
+            )
+          , (5, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "qüinqua"
+                       CtxMul _ (Lit 100) _ → "qüin"
+                       _                    → "quint" ⊕ postFix
+            )
+          , (6, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "sexa"
+                       CtxMul _ (Lit 100) _ → "sex"
+                       _                    → "sext" ⊕ postFix
+            )
+          , (7, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "septua"
+                       CtxMul _ (Lit 100) _ → "setin"
+                       _                    → "sétim" ⊕ postFix
+            )
+          , (8, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "octo"
+                       CtxMul _ (Lit 100) _ → "octin"
+                       _                    → "oitav" ⊕ postFix
+            )
+          , (9, \c → case c of
+                       CtxMul _ (Lit 10)  _ → "nona"
+                       CtxMul _ (Lit 100) _ → "non"
+                       _                    → "non" ⊕ postFix
+            )
+          , (10, \c → case c of
+                        CtxAdd R (Lit _) _  → "ze"
+                        CtxMul R (Lit _) _
+                            | isOutside R c → "gésim" ⊕ postFix
+                            | otherwise     → "gésimo"
+                        _ | isOutside R c   → "décim" ⊕ postFix
+                          | otherwise       → "décimo"
+            )
+          , (100, \c → case c of
+                         CtxAdd {}         → "cento"
+                         CtxMul _ (Lit n) _
+                             | n ∉ [2,3,6] → "gentésim" ⊕ postFix
+                         _                 → "centésim" ⊕ postFix
+            )
+          , (1000, const $ "milésim" ⊕ postFix)
+          ]
+          where
+            postFix = if G.isFeminine inf
+                      then if G.isSingular inf
+                           then "a"
+                           else "as"
+                      else if G.isSingular inf
+                           then "o"
+                           else "os"
 
 shortScaleRepr ∷ (IsString s, Monoid s)
                ⇒ i → ℤ → ℤ → Exp i → Ctx (Exp i) → Maybe s

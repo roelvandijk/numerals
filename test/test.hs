daf67e68afe33e8f0ed370e16fb0236fcd219c50
Roel van Dijk <vandijk.roel@gmail.com>
2011-10-10 21:07:50 +0200

    Made test suite polymorphic to support different types of test data. To prevent ambiguous types all test data now has explicit type signatures.

--- test/test.hs
+++ test/test.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE NoImplicitPrelude, PackageImports, UnicodeSyntax #-}
+{-# LANGUAGE NoImplicitPrelude
+           , PackageImports
+           , ScopedTypeVariables
+           , UnicodeSyntax
+  #-}
 
 module Main where
 
@@ -14,7 +18,7 @@ import "base" Data.Maybe    ( Maybe(Just), fromMaybe )
 import "base" Prelude       ( String )
 import "base" System.IO     ( IO )
 import "base" Text.Printf   ( printf )
-import "base" Text.Show     ( show )
+import "base" Text.Show     ( Show, show )
 import "base-unicode-symbols" Data.Eq.Unicode       ( (≢) )
 import "base-unicode-symbols" Data.Function.Unicode ( (∘) )
 import "base-unicode-symbols" Prelude.Unicode       ( ℤ )
@@ -96,93 +100,84 @@ main ∷ IO ()
 main = defaultMain tests
 
 tests ∷ [Test]
-tests = [ testGroup "AF"
-          [ mkTests "cardinal" AF.cardinal AF.cardinals
-          , mkTests "ordinal"  AF.ordinal  AF.ordinals
-          ]
-        , testGroup "AMP" [mkTests "cardinal" AMP.cardinal AMP.cardinals]
-        , testGroup "CHN" [mkTests "cardinal" CHN.cardinal CHN.cardinals]
-        , testGroup "CHR" [mkTests "cardinal" CHR.cardinal CHR.cardinals]
-        , testGroup "CS" [mkTests "cardinal" CS.cardinal CS.cardinals]
-        , testGroup "DE"
-          [ mkTests "cardinal" DE.cardinal DE.cardinals
-          , mkTests "ordinal"  DE.ordinal  DE.ordinals
-          ]
-        , testGroup "EN"
-          [ testGroup "UK"
-            [ mkTests "cardinal" EN.uk_cardinal EN.uk_cardinals
-            , mkTests "ordinal"  EN.uk_ordinal  EN.uk_ordinals
-            ]
-          , testGroup "US"
-            [ mkTests "cardinal" EN.us_cardinal EN.us_cardinals
-            , mkTests "ordinal"  EN.us_ordinal  EN.us_ordinals
-            ]
-          ]
-        , testGroup "EO"  [mkTests "cardinal" EO.cardinal EO.cardinals]
-        , testGroup "ES"  [mkTests "cardinal" ES.cardinal ES.cardinals]
-        , testGroup "FR"
-          [ mkTests "cardinal" FR.cardinal FR.cardinals
-          , mkTests "ordinal"  FR.ordinal  FR.ordinals
-          ]
-        , testGroup "GSW" [mkTests "cardinal" GSW.cardinal GSW.cardinals]
-        , testGroup "GV"  [mkTests "cardinal" GV.cardinal GV.cardinals]
-        , testGroup "HE"  [mkTests "cardinal" HE.cardinal HE.cardinals]
-        , testGroup "IT"
-          [ mkTests "cardinal" IT.cardinal IT.cardinals
-          , mkTests "ordinal"  IT.ordinal  IT.ordinals
-          ]
-        , testGroup "JA"
-          [ testGroup "preferred"
-            [mkTests "cardinal" JA.preferred_cardinal
-                                JA.preferred_cardinals
-            ]
-          , testGroup "kanji"
-            [mkTests "cardinal" JA.kanji_cardinal JA.kanji_cardinals]
-          , testGroup "daiji"
-            [mkTests "cardinal" JA.daiji_cardinal JA.daiji_cardinals]
-          ]
-        , testGroup "LA"  [mkTests "cardinal" LA.cardinal LA.cardinals]
-        , testGroup "MG"  [mkTests "cardinal" MG.cardinal MG.cardinals]
-        , testGroup "NL"  [ mkTests "cardinal" NL.cardinal NL.cardinals
-                          , mkTests "ordinal"  NL.ordinal  NL.ordinals
-                          ]
-        , testGroup "NO"  [mkTests "cardinal" NO.cardinal  NO.cardinals]
-        , testGroup "NQM" [mkTests "cardinal" NQM.cardinal NQM.cardinals]
-        , testGroup "OJ"  [mkTests "cardinal" OJ.cardinal  OJ.cardinals]
-        -- -- , testGroup "PAA" [mkTests "cardinal" PAA.cardinal PAA.cardinals]
-        , testGroup "PL"  [mkTests "cardinal" PL.cardinal PL.cardinals]
-        , testGroup "PT"  [ mkTests "cardinal" PT.cardinal PT.cardinals
-                          , mkTests "ordinal"  PT.ordinal  PT.ordinals
-                          ]
-        , testGroup "RU"  [mkTests "cardinal" RU.cardinal RU.cardinals]
-        , testGroup "SCO" [mkTests "cardinal" SCO.cardinal SCO.cardinals]
-        , testGroup "SV"  [mkTests "cardinal" SV.cardinal SV.cardinals]
-        , testGroup "TR"  [mkTests "cardinal" TR.cardinal TR.cardinals]
-        , testGroup "WO"  [mkTests "cardinal" WO.cardinal WO.cardinals]
-        , testGroup "YOR" [mkTests "cardinal" YOR.cardinal YOR.cardinals]
-        , testGroup "ZH"
-          [ testGroup "characters (traditional)"
-            [mkTests "cardinal" ZH.trad_cardinal
-                                ZH.trad_cardinals
-            ]
-          , testGroup "characters (simplified)"
-            [mkTests "cardinal" ZH.simpl_cardinal
-                                ZH.simpl_cardinals
-            ]
-          , testGroup "financial characters (traditional)"
-            [mkTests "cardinal" ZH.finance_trad_cardinal
-                                ZH.finance_trad_cardinals
-            ]
-          , testGroup "financial characters (simplified)"
-            [mkTests "cardinal" ZH.finance_simpl_cardinal
-                                ZH.finance_simpl_cardinals
-            ]
-          , testGroup "pinyin"
-            [mkTests "cardinal" ZH.pinyin_cardinal
-                                ZH.pinyin_cardinals
-            ]
-          ]
-        ]
+tests =
+  [ testGroup "AF"
+    [ mkTests "cardinal" AF.cardinal (AF.cardinals ∷ TestData ℤ String)
+    , mkTests "ordinal"  AF.ordinal  (AF.ordinals  ∷ TestData ℤ String)
+    ]
+  , testGroup "AMP" [mkTests "cardinal" AMP.cardinal (AMP.cardinals ∷ TestData ℤ String)]
+  , testGroup "CHN" [mkTests "cardinal" CHN.cardinal (CHN.cardinals ∷ TestData ℤ String)]
+  , testGroup "CHR" [mkTests "cardinal" CHR.cardinal (CHR.cardinals ∷ TestData ℤ String)]
+  , testGroup "CS"  [mkTests "cardinal" CS.cardinal  (CS.cardinals  ∷ TestData ℤ String)]
+  , testGroup "DE"
+    [ mkTests "cardinal" DE.cardinal (DE.cardinals ∷ TestData ℤ String)
+    , mkTests "ordinal"  DE.ordinal  (DE.ordinals  ∷ TestData ℤ String)
+    ]
+  , testGroup "EN"
+    [ testGroup "UK"
+      [ mkTests "cardinal" EN.uk_cardinal (EN.uk_cardinals ∷ TestData ℤ String)
+      , mkTests "ordinal"  EN.uk_ordinal  (EN.uk_ordinals  ∷ TestData ℤ String)
+      ]
+    , testGroup "US"
+      [ mkTests "cardinal" EN.us_cardinal (EN.us_cardinals ∷ TestData ℤ String)
+      , mkTests "ordinal"  EN.us_ordinal  (EN.us_ordinals  ∷ TestData ℤ String)
+      ]
+    ]
+  , testGroup "EO"  [mkTests "cardinal" EO.cardinal (EO.cardinals ∷ TestData ℤ String)]
+  , testGroup "ES"  [mkTests "cardinal" ES.cardinal (ES.cardinals ∷ TestData ℤ String)]
+  , testGroup "FR"
+    [ mkTests "cardinal" FR.cardinal (FR.cardinals ∷ TestData ℤ String)
+    , mkTests "ordinal"  FR.ordinal  (FR.ordinals  ∷ TestData ℤ String)
+    ]
+  , testGroup "GSW" [mkTests "cardinal" GSW.cardinal (GSW.cardinals ∷ TestData ℤ String)]
+  , testGroup "GV"  [mkTests "cardinal" GV.cardinal  (GV.cardinals  ∷ TestData ℤ String)]
+  , testGroup "HE"  [mkTests "cardinal" HE.cardinal  (HE.cardinals  ∷ TestData ℤ String)]
+  , testGroup "IT"
+    [ mkTests "cardinal" IT.cardinal (IT.cardinals ∷ TestData ℤ String)
+    , mkTests "ordinal"  IT.ordinal  (IT.ordinals  ∷ TestData ℤ String)
+    ]
+  , testGroup "JA"
+    [ testGroup "preferred"
+      [mkTests "cardinal" JA.preferred_cardinal
+                          (JA.preferred_cardinals ∷ TestData ℤ String)
+      ]
+    , testGroup "kanji"
+      [mkTests "cardinal" JA.kanji_cardinal (JA.kanji_cardinals ∷ TestData ℤ String)]
+    , testGroup "daiji"
+      [mkTests "cardinal" JA.daiji_cardinal (JA.daiji_cardinals ∷ TestData ℤ String)]
+    ]
+  , testGroup "LA"  [mkTests "cardinal" LA.cardinal (LA.cardinals ∷ TestData ℤ String)]
+  , testGroup "MG"  [mkTests "cardinal" MG.cardinal (MG.cardinals ∷ TestData ℤ String)]
+  , testGroup "NL"  [ mkTests "cardinal"  NL.cardinal  (NL.cardinals ∷ TestData ℤ String)
+                    , mkTests "ordinal"   NL.ordinal   (NL.ordinals ∷ TestData ℤ String)
+                    ]
+  , testGroup "NO"  [mkTests "cardinal" NO.cardinal  (NO.cardinals  ∷ TestData ℤ String)]
+  , testGroup "NQM" [mkTests "cardinal" NQM.cardinal (NQM.cardinals ∷ TestData ℤ String)]
+  , testGroup "OJ"  [mkTests "cardinal" OJ.cardinal  (OJ.cardinals  ∷ TestData ℤ String)]
+  -- , testGroup "PAA" [mkTests "cardinal" PAA.cardinal (PAA.cardinals ∷ TestData ℤ String)]
+  , testGroup "PL"  [mkTests "cardinal" PL.cardinal  (PL.cardinals  ∷ TestData ℤ String)]
+  , testGroup "PT"  [ mkTests "cardinal" PT.cardinal (PT.cardinals  ∷ TestData ℤ String)
+                    , mkTests "ordinal"  PT.ordinal  (PT.ordinals   ∷ TestData ℤ String)
+                    ]
+  , testGroup "RU"  [mkTests "cardinal" RU.cardinal  (RU.cardinals  ∷ TestData ℤ String)]
+  , testGroup "SCO" [mkTests "cardinal" SCO.cardinal (SCO.cardinals ∷ TestData ℤ String)]
+  , testGroup "SV"  [mkTests "cardinal" SV.cardinal  (SV.cardinals  ∷ TestData ℤ String)]
+  , testGroup "TR"  [mkTests "cardinal" TR.cardinal  (TR.cardinals  ∷ TestData ℤ String)]
+  , testGroup "WO"  [mkTests "cardinal" WO.cardinal  (WO.cardinals  ∷ TestData ℤ String)]
+  , testGroup "YOR" [mkTests "cardinal" YOR.cardinal (YOR.cardinals ∷ TestData ℤ String)]
+  , testGroup "ZH"
+    [ testGroup "characters (traditional)"
+      [mkTests "cardinal" ZH.trad_cardinal (ZH.trad_cardinals ∷ TestData ℤ String)]
+    , testGroup "characters (simplified)"
+      [mkTests "cardinal" ZH.simpl_cardinal (ZH.simpl_cardinals ∷ TestData ℤ String)]
+    , testGroup "financial characters (traditional)"
+      [mkTests "cardinal" ZH.finance_trad_cardinal (ZH.finance_trad_cardinals ∷ TestData ℤ String)]
+    , testGroup "financial characters (simplified)"
+      [mkTests "cardinal" ZH.finance_simpl_cardinal (ZH.finance_simpl_cardinals ∷ TestData ℤ String)]
+    , testGroup "pinyin"
+      [mkTests "cardinal" ZH.pinyin_cardinal (ZH.pinyin_cardinals ∷ TestData ℤ String)]
+    ]
+  ]
 
 
 --------------------------------------------------------------------------------
@@ -203,24 +198,31 @@ testBounds name conversion bounds@(lo, hi) =
       checkConv = isJust ∘ conversion
 -}
 
-mkTests ∷ String
-        → (Inflection → ℤ → Maybe String)
-        → TestData ℤ String
+mkTests ∷ ∀ α
+        . (Show α)
+        ⇒ String
+        → (Inflection → α → Maybe String)
+        → TestData α String
         → Test
 mkTests name f = testGroup name ∘ map perInflection
     where
-      perInflection ∷ (String, Inflection, [(ℤ, String)]) → Test
+      perInflection ∷ (String, Inflection, [(α, String)]) → Test
       perInflection (infName, inf, xs) = testGroup infName $ map (perValue inf) xs
 
-      perValue ∷ Inflection → (ℤ, String) → Test
+      perValue ∷ Inflection → (α, String) → Test
       perValue inf (n, s) = testCase (show n) $ testConversion f inf n s
 
-testConversion ∷ (Inflection → ℤ → Maybe String) → Inflection → ℤ → String → Assertion
+testConversion ∷ (Show α)
+               ⇒ (Inflection → α → Maybe String)
+               → Inflection
+               → α
+               → String
+               → Assertion
 testConversion f inf n s =
   let r = f inf n
   in when (r ≢ Just s)
         $ assertFailure
-        $ printf "Expected %i = \"%s\" but got \"%s\""
-                 n
+        $ printf "Expected %s = \"%s\" but got \"%s\""
+                 (show n)
                  s
                  (fromMaybe "no result" r)
